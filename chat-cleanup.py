346c109f096a3eec139cac6c7e635552045001bb
Sean Parker <sean.parker067@gmail.com>
2018-09-25 17:07:33 +0100

    Add comments to cleanup code

--- chat-cleanup.py
+++ chat-cleanup.py
@@ -22,18 +22,25 @@ def parseLineByLine(lines):
     return formatted, names
 
 def filterToTwo(names):
+    #We can extend the program later to accept more than two people
+    #for now, we take the two most common people from the chat and reject other
+    #names that may exist
     remain = names
     if (len(names) > 2):
         min_val = min(names.values())
         remain = names.keys() - (k for k, v in names.items() if v == min_val)
     return remain
 
+#Remove line terminators from each line when it is read
 lines = (line.rstrip('\n') for line in open("_chat 2.txt"))
 newLines, names = parseLineByLine(lines)
 names = filterToTwo(names)
 
+#Finally, we write the output to a csv file
 with open('formatted_chat1.csv', "w") as chat_file:
     chat_writer = csv.writer(chat_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for l in newLines:
+        #Reject other names, only accept those in the names list
         if (l[1] in names):
+            #Write the tuple to the csv file
             chat_writer.writerow([l[0], l[1], l[2]])
